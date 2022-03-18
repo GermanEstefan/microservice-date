58944810d2ddd950860563068d69410fee68fcba
German <german.estefan81@gmail.com>
2022-03-18 17:27:59 -0300

    Function jsonBody added

--- dist/main.js
+++ dist/main.js
@@ -8,25 +8,26 @@ const dotenv_1 = require("dotenv");
 (0, dotenv_1.config)();
 const expressApp = (0, express_1.default)();
 const PORT = process.env.PORT;
+const jsonBody = (res, unixDate, utcDate) => {
+    return res.json({
+        unix: unixDate,
+        utc: utcDate
+    });
+};
+expressApp.get('/api/', (req, res) => {
+    return jsonBody(res, new Date().getTime(), new Date().toUTCString());
+});
 expressApp.get('/api/:date', (req, res) => {
     const { date } = req.params;
     if (!Number(date)) { //El parametro viene en formato de cadena
-        if (!Date.parse(date)) { //Es un date de formato de cadena INVALIDO
+        if (!Date.parse(date)) { //Verificamos si el formato de cadena es VALIDO
             return res.json({
                 error: 'Invalid Date'
             });
         }
-        return res.json({
-            unix: Date.parse(date),
-            utc: new Date(date).toUTCString()
-        });
+        return jsonBody(res, Date.parse(date), new Date(date).toUTCString());
     }
-    else {
-        //El parametro viene en formato de entero
-        return res.json({
-            unix: new Date(Number(date)).getTime(),
-            utc: new Date(Number(date)).toUTCString()
-        });
-    }
+    //El parametro viene en formato de entero
+    return jsonBody(res, new Date(Number(date)).getTime(), new Date(Number(date)).toUTCString());
 });
 expressApp.listen(PORT, () => console.log('Server is running in the port ', PORT));
--- main.ts
+++ main.ts
@@ -1,32 +1,38 @@
 import express from 'express';
+import { Response } from 'express';
 import { config } from 'dotenv';
 config();
 
 const expressApp = express();
 const PORT = process.env.PORT;
 
+const jsonBody = (res: Response, unixDate: number, utcDate: string) => {
+    return res.json({
+        unix: unixDate,
+        utc: utcDate
+    })
+}
+
+expressApp.get('/api/', (req, res) => {
+    return jsonBody(res, new Date().getTime(), new Date().toUTCString())
+})
+
 expressApp.get('/api/:date', (req, res) => {
 
     const { date } = req.params;
+    if (!Number(date)) { //El parametro viene en formato de cadena
 
-    if(!Number(date)){ //El parametro viene en formato de cadena
-        if(!Date.parse(date) ){ //Es un date de formato de cadena INVALIDO
+        if (!Date.parse(date)) { //Verificamos si el formato de cadena es VALIDO
             return res.json({
                 error: 'Invalid Date'
             })
         }
-        return res.json({ //Es un formato de cadena valido
-            unix: Date.parse(date),
-            utc: new Date(date).toUTCString()
-        })
-    }else {
-        //El parametro viene en formato de entero
-        return res.json({
-            unix: new Date(Number(date)).getTime(),
-            utc: new Date(Number(date)).toUTCString()
-        })
+
+        return jsonBody(res, Date.parse(date), new Date(date).toUTCString())
     }
 
+    //El parametro viene en formato de entero
+    return jsonBody(res, new Date(Number(date)).getTime(), new Date(Number(date)).toUTCString())
 
 });
 
